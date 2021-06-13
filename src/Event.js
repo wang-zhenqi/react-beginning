80fba34194ecc9cc49eedbdc55da975833e0f7ca
Zhenqi Wang <zhenqi.wang@thoughtworks.com>
2021-06-14 00:03:38 +0800

    Event Object

--- src/Event.js
+++ src/Event.js
@@ -18,14 +18,19 @@ class EventBinding extends React.Component {
         )
     }
 }
-
+/*
+用函数的方式返回组件也是一样，可以对组件绑定事件
+还可通过事件对象（e）来控制事件触发时的行为，事件处理函数的参数就是一个事件对象
+React的事件对象也被称作合成事件对象，在各个浏览器中的表现均相同
+ */
 function FunctionButton() {
-    function HandleClick() {
-        prompt("function button clicked")
+    function HandleClick(e) {
+        e.preventDefault();
+        prompt("function link clicked")
     }
 
     return(
-        <button onClick={HandleClick}>I'm a button by function</button>
+        <a href={"baidu.com"} onClick={HandleClick}>I'm a link created by a function</a>
     )
 }
 
